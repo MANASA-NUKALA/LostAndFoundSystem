e7d3d37522a3a5d1f39d356d1a6e8410f3b2da3b
MANASA-NUKALA <167613166+MANASA-NUKALA@users.noreply.github.com>
2025-06-26 00:14:23 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,13 @@
 @echo off
 echo Compiling all Java files...
-javac -cp .;lib\mysql-connector-j-9.3.0.jar */*.java
+
+javac -cp .;lib\mysql-connector-j-9.3.0.jar client\*.java interfaces\*.java model\*.java server\*.java
+
 if %errorlevel% neq 0 (
     echo Compilation failed!
     pause
     exit /b %errorlevel%
 )
+
 echo Compilation successful!
 pause
